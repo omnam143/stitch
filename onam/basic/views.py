3f0427008fde7dcef68b0453563d6f60ee713f83
Agnes <contactopenhealth@gmail.com>
2014-03-07 00:11:12 +0400

    json

--- onam/basic/views.py
+++ onam/basic/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from basic.models import Country, City
 
-# Create your views here.
+def index(request):
+    #json_serializer = serializers.get_serializer("json")()
+    #json_serializer.serialize(Country.objects.all(), ensure_ascii=False, stream=response)
+    #data = serializers.serialize("json", Country.objects.all())
+    #return HttpResponse(json_serializer)
+    data = serializers.serialize("json", City.objects.all())
+    return HttpResponse(data)
+
+    
+    
+
