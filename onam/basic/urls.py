29a94e3b1b13708fbb16573214f2b0ca2fb47ef2
nitika26 <nitika.iitd@gmail.com>
2014-03-11 00:34:09 +0400

    autcomplete

--- onam/basic/urls.py
+++ onam/basic/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import patterns, url
 from basic import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index')
+    url(r'^(?P<alphabet>\w)/$', views.index, name='index'),
+    url(r'^ind/$', views.ind, name='ind')
 )
--- onam/basic/views.py
+++ onam/basic/views.py
@@ -1,16 +1,22 @@
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from basic.models import Country, City
+import json
 
-def index(request):
-    #json_serializer = serializers.get_serializer("json")()
-    #json_serializer.serialize(Country.objects.all(), ensure_ascii=False, stream=response)
-    #data = serializers.serialize("json", Country.objects.all())
-    #return HttpResponse(json_serializer)
+
+def index(request,alphabet):
+    #data = serializers.serialize("json", Country.objects.filter(name__startswith=alphabet))
+    #return HttpResponse(data)
     data = serializers.serialize("json", City.objects.all())
     return HttpResponse(data)
+    #return HttpResponse(json.dumps(data), content_type="application/json")
 
     
+def ind(request):
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    return render_to_response('basic/typeaheadsimple.html', context_dict, context)
     
-
