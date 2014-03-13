73f95d76849be55eb5c15971e7581c95c77f3d62
rishabh-g <rishabh-personal@Rishabhs-MacBook-Pro.local>
2014-03-13 04:49:25 +0100

    Updated default page to show autocomplete page

--- onam/onam/urls.py
+++ onam/onam/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from basic import views
 from django.contrib import admin
 admin.autodiscover()
 
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'onam.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', views.ind, name='main'),
     url(r'^basic/', include('basic.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
